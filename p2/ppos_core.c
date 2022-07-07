5cff6d8193f5b1fad97aafc471ed4fba330a6e76
Pull-eckermann <eric7880@gmail.com>
2022-07-06 23:25:48 -0300

    p2 terminado

--- p2/ppos_core.c
+++ p2/ppos_core.c
@@ -1,5 +1,6 @@
 //ERICK ECKERMANN CARDOSO GRR20186075
 #include <stdlib.h>
+#include <stdio.h>
 #include <ucontext.h>
 #include "ppos_data.h"
 #include "ppos.h"
@@ -7,21 +8,27 @@
 #define STACKSIZE 64*1024	/* tamanho de pilha das threads */
 
 int cont_id = 0;
-task_t *main, *current;
+task_t main_cont, *current;
 
 // Inicializa o sistema operacional; deve ser chamada no inicio do main()
 void ppos_init (){
-  getcontext(&main->context);
-  main->id = 0;
-  //setvbuf (stdout, 0, _IONBF, 0);
+  getcontext(&main_cont.context);         //Salva o contexto da tarefa main
+  main_cont.id = 0;
+  main_cont.next = NULL;
+  main_cont.prev = NULL;  
+  current = &main_cont;                   //Tarefa corrente aponta para main
+  setvbuf (stdout, 0, _IONBF, 0);         //desativa o buffer da saida padrao (stdout), usado pela função printf
 }
 
 // Cria uma nova tarefa. Retorna um ID> 0 ou erro.
 int task_create (task_t *task, void (*start_func)(void *), void *arg){
   char *stack ;
+  task->next = NULL;
+  task->prev = NULL;
   getcontext(&task->context);
-  stack = malloc (STACKSIZE) ;
+
   //Cria a pilha da tarefa
+  stack = malloc (STACKSIZE) ;
   if (stack){
     task->context.uc_stack.ss_sp = stack ;
     task->context.uc_stack.ss_size = STACKSIZE ;
@@ -33,25 +40,35 @@ int task_create (task_t *task, void (*start_func)(void *), void *arg){
     exit (1) ;
   }
   //Seta parâmetros do contexto
+  current = task;
   makecontext(&task->context, (void*) (*start_func), 1, arg);
+
   //Soma 1 no contador do ID e atribui esse id à tarefa criada
   cont_id ++;
   task->id = cont_id;
+  //Tarefa corrente volta a ser a main
+  current = &main_cont;
+
   return cont_id;
 }
 
-// Termina a tarefa corrente, indicando um valor de status encerramento
+// Termina a tarefa corrente voltando a main, indicando um valor de status encerramento
 void task_exit (int exit_code){
-  swapcontext(&current->context, &main->context);
-  printf("%d: fim\n");
+  swapcontext(&current->context, &main_cont.context);
+  printf("%d: fim\n",exit_code);
 }
 
 // alterna a execução para a tarefa indicada
 int task_switch (task_t *task){
-  swapcontext(&current->context, &task->context);
+  task_t *atual;
+  atual = current;
+  current = task;
+  swapcontext(&atual->context, &task->context);
+
+  return 0; 
 } 
 
-// retorna o identificador da tarefa corrente (main deve ser 0)
+// retorna o identificador da tarefa corrente (main_cont deve ser 0)
 int task_id (){
-  
+  return current->id;
 }
